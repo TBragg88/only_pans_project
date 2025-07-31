8a48a6cdf699fb268a954a6fa97b1687d4703eaf
Tom Bragg <tbragg88@hotmail.com>
2025-07-31 15:37:46 +0100

    Add user authentication URLs and enhance base template with navigation and modals

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),  # Homepage
     path('recipes/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path('create/', views.recipe_create, name='recipe_create'),
 ]
