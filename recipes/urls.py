e926891d9d6f3e022d329f1f3c95281a26064bbb
Tom Bragg <tbragg88@hotmail.com>
2025-08-01 01:48:57 +0100

    Refactor URL configurations in accounts and recipes apps; remove app_name declarations for clarity

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,8 +6,6 @@ from django.urls import path
 
 from . import views
 
-app_name = 'recipes'
-
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),  # Homepage
     path('test/', views.test_view, name='test_view'),  # Test view
